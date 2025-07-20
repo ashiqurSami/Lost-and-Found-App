4b1add3141f05b0635d990d31ac84ed94960beec
ashiqurSami <samitaseen@gmail.com>
2025-07-20 06:01:19 +0600

    feat(match) : add matching based category,name,descriptiona and location (within 3 km)

--- item/urls.py
+++ item/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import LostFoundItemViewSet
+from .views import LostFoundItemViewSet, MatchFoundItemView
 
 router = DefaultRouter()
 router.register('items', LostFoundItemViewSet, basename='item')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('items/match/<int:lost_item_id>/',MatchFoundItemView.as_view(), name='match-found-items'),
+]
